cb0b0684ff269e383e63543cd62c92a3920a4927
AnnaSkoda <zbaraz77@gmail.com>
2024-02-13 21:37:00 +0200

    add new section

--- js/main.js
+++ js/main.js
@@ -9,4 +9,14 @@ $(document).ready(function () {
 
         nextArrow: '<button class="btn slider-btn slider-next"><img src="img/svg/arrow-down.svg" alt="arrow-down" /></button>',
     })
-});
+
+    $('.video-sliders').slick({
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+
+        prevArrow: '<button class="btn-learn slider-btn-learn slider-prev-video"><img src="img/svg/arrow-left.svg" alt="arrow-left" /></button>',
+        nextArrow: '<button class="btn-learn slider-btn-learn slider-next-video"><img src="img/svg/arrow-right.svg" alt="arrow-right" /></button>',
+    })
+});s
